802ce811783f5e1f481f97a96dcb2ea48940a6ab
Saulo Antunes <sauloantuness@gmail.com>
2016-06-13 16:59:18 -0300

    Fix update uri solutions all

---
+++ bang/home/management/commands/updateUriSolutionsAll.py
@@ -0,0 +1,41 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from home.models import Profile, Problem, Solution
+from .uri import Uri
+
+class Command(BaseCommand):
+	def handle(self, *args, **options):
+		def createSolution(profile, problem, date):
+			solution = Solution()
+			solution.profile = profile
+			solution.problem = problem
+			solution.date = date
+			solution.save()
+
+		for profile in Profile.objects.all():
+			print(profile.name)
+			uri = Uri(profile.uriId)
+
+			while True:
+				solutions = uri.getSolutions()
+
+				if not solutions:
+					break
+
+				for s in solutions:
+					try:
+						problem = Problem.objects.get(code=s['code'], judge='uri')
+					except Problem.DoesNotExist:
+						print('URI is missing problem: ', s['code'])
+						continue
+					try:
+						solution = Solution.objects.get(problem=problem, profile=profile)
+						uri.done = True
+					except Solution.DoesNotExist:
+						createSolution(profile, problem, s['date'])
+						uri.new += 1
+						
+					if uri.done:
+						break
+					
+			print("New Solutions: ", uri.new, "\n")
