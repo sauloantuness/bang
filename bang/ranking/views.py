97541eee30e0823700c92f94976e79f76646ad31
Saulo Antunes <sauloantuness@gmail.com>
2017-01-22 12:37:04 -0200

    Ranking: remove contest and add score

--- bang/ranking/views.py
+++ bang/ranking/views.py
@@ -2,27 +2,29 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from home.models import Profile, Solution
 from django.contrib.auth.models import User
+from functools import reduce
 # Create your views here.
 
 @login_required
-def ranking(request, order='all'):
-	profiles = []
+def ranking(request, order='score'):
+    profiles = []
 
-	for p in Profile.objects.all():
-		profile = {
-			'profile' : p,
-			'name' : p.name,
-			'uri'  : Solution.objects.filter(problem__judge='uri', profile=p).count(),
-			'uva'  : Solution.objects.filter(problem__judge='uva', profile=p).count(),
-			'spoj' : Solution.objects.filter(problem__judge='spoj', profile=p).count(),
-			'contests' : 0,
-			'all' : 0,
-		}
-		profile['all'] = profile['uri'] + profile['uva'] + profile['spoj']
-		profiles.append(profile)
+    for p in Profile.objects.all():
+        profile = {
+            'profile': p,
+            'name': p.name,
+            'uri': Solution.objects.filter(problem__judge='uri', profile=p).count(),
+            'uva': Solution.objects.filter(problem__judge='uva', profile=p).count(),
+            'spoj': Solution.objects.filter(problem__judge='spoj', profile=p).count(),
+            'total': 0,
+            'score': 0,
+        }
+        profile['total'] = profile['uri'] + profile['uva'] + profile['spoj']
+        profile['score'] = reduce(lambda acc, s: s.problem.level + acc, p.solution_set.all(), 0)
+        profiles.append(profile)
 
-	context = {
-		'profiles' : sorted(profiles, key=lambda k: k[order], reverse=(order != 'name'))
-	}
+    context = {
+        'profiles' : sorted(profiles, key=lambda k: k[order], reverse=(order != 'name'))
+    }
 
-	return render(request, 'ranking/ranking.html', context)
+    return render(request, 'ranking/ranking.html', context)
